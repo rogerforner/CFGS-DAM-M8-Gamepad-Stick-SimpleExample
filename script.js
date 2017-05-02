67a53097003b333ccfddcd6a9d6e2868350f3dde
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-05-02 04:35:03 +0200

    2 Gampads Ok :)

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 var ball;
 
 window.addEventListener("gamepadconnected", function(e) {
-
+    if(ball){
+        ball2 = document.getElementById("ball2");
+        ball2.style.backgroundColor = "purple";
+        document.getElementsId("p2")[0].innerHTML = e.gamepad.id;
+    }
     ball = document.getElementById("ball");
     ball.style.backgroundColor = "green";
     document.getElementsByTagName("p")[0].innerHTML = e.gamepad.id;
@@ -16,10 +20,17 @@ function updateLoop() {
     var left = (gp.axes[0] + 1) / 2 * (window.innerWidth - ball.offsetWidth);
     var right = (gp.axes[1] + 1) / 2 * (window.innerHeight - ball.offsetHeight);
 
+    var gp2 = navigator.getGamepads()[1];
+    var left2 = (gp2.axes[0] + 1) / 2 * (window.innerWidth - ball2.offsetWidth);
+    var right2 = (gp2.axes[1] + 1) / 2 * (window.innerHeight - ball2.offsetHeight);
+
     ball.style.left = left + "px";
     ball.style.top =  right + "px";
 
-    if (gp.buttons[0].pressed) {
+    ball2.style.left = left2 + "px";
+    ball2.style.top =  right2 + "px";
+
+    if (gp.buttons[0].pressed || gp2.buttons[0].pressed) {
         document.body.style.backgroundColor = "red";
     } else {
         document.body.style.backgroundColor = "white";
